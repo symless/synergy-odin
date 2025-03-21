152195e847aba2c8ebd1d9a8fb628ef984427511
Nick Bolton <nick@symless.com>
2025-03-21 12:06:43 +0000

    Update MSVC merge module detection to improve path handling and logging

--- cmake/Packaging.cmake
+++ cmake/Packaging.cmake
@@ -66,18 +66,15 @@ macro(configure_windows_packaging)
   # Get the merge module (MSM) from the dev env.
   # This is deprecated and we should switch to MSI when possible.
   # Docs: https://learn.microsoft.com/en-us/cpp/windows/redistributing-components-by-using-merge-modules?view=msvc-170
-  set(VCINSTALLDIR $ENV{VCINSTALLDIR})
-  message(STATUS "VCINSTALLDIR: ${VCINSTALLDIR}")
-
-  set(REDIST_DIR "${VCINSTALLDIR}/Redist/MSVC/v143")
-  message(STATUS "Redist dir: ${REDIST_DIR}")
-
-  set(REDIST_MERGE_MODULE_DIR "${VCINSTALLDIR}Redist\\MSVC\\v143\\MergeModules")
-  file(GLOB_RECURSE REDIST_MERGE_MODULE_PATHS "${REDIST_MERGE_MODULE_DIR}\\*.msm")
-  message(STATUS "Redist merge module paths: ${REDIST_MERGE_MODULE_PATHS}")
-  
-  if (NOT REDIST_MERGE_MODULE_PATHS)
-    message(FATAL_ERROR "No merge modules found in ${REDIST_MERGE_MODULE_DIR}")
+  set(REDIST_MERGE_MODULE_DIR "$ENV{VCINSTALLDIR}Redist/MSVC/v143/MergeModules")
+  message(STATUS "MSVC merge module dir: ${REDIST_MERGE_MODULE_DIR}")
+  file(GLOB REDIST_MERGE_MODULE_PATHS "${REDIST_MERGE_MODULE_DIR}/Microsoft_VC143_CRT_x86.msm")
+  if (REDIST_MERGE_MODULE_PATHS)
+    message(STATUS "MSVC merge module paths: ${REDIST_MERGE_MODULE_PATHS}")
+    list(GET REDIST_MERGE_MODULE_PATHS 0 REDIST_MERGE_MODULE_PATH)
+    message(STATUS "MSVC merge module found: ${REDIST_MERGE_MODULE_PATH}")
+  else()
+    message(WARNING "MSVC merge module not found")
   endif()
 
   # use first path
