1959ffaaf6f3841b6a5e34b8fe76b50c16d7d33d
Nick Bolton <nick@symless.com>
2025-03-21 12:07:41 +0000

    Change logging level for MSVC merge module paths to verbose

--- cmake/Packaging.cmake
+++ cmake/Packaging.cmake
@@ -67,19 +67,15 @@ macro(configure_windows_packaging)
   # This is deprecated and we should switch to MSI when possible.
   # Docs: https://learn.microsoft.com/en-us/cpp/windows/redistributing-components-by-using-merge-modules?view=msvc-170
   set(REDIST_MERGE_MODULE_DIR "$ENV{VCINSTALLDIR}Redist/MSVC/v143/MergeModules")
-  message(STATUS "MSVC merge module dir: ${REDIST_MERGE_MODULE_DIR}")
+  message(VERBOSE "MSVC merge module dir: ${REDIST_MERGE_MODULE_DIR}")
   file(GLOB REDIST_MERGE_MODULE_PATHS "${REDIST_MERGE_MODULE_DIR}/Microsoft_VC143_CRT_x86.msm")
   if (REDIST_MERGE_MODULE_PATHS)
-    message(STATUS "MSVC merge module paths: ${REDIST_MERGE_MODULE_PATHS}")
+    message(VERBOSE "MSVC merge module paths: ${REDIST_MERGE_MODULE_PATHS}")
     list(GET REDIST_MERGE_MODULE_PATHS 0 REDIST_MERGE_MODULE_PATH)
     message(STATUS "MSVC merge module found: ${REDIST_MERGE_MODULE_PATH}")
   else()
     message(WARNING "MSVC merge module not found")
   endif()
-
-  # use first path
-  list(GET REDIST_MERGE_MODULE_PATHS 0 REDIST_MERGE_MODULE_PATH)
-  message(STATUS "Redist merge module path: ${REDIST_MERGE_MODULE_PATH}")
 
   configure_files(${PROJECT_SOURCE_DIR}/res/dist/wix
                   ${PROJECT_BINARY_DIR}/installer)
